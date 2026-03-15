8d6ba588acb569f12cac7d4d349bc740168b1c37
Siri Bathula <siribathula8228@gmail.com>
2026-03-15 21:01:43 +0530

    Connect frontend to deployed backend

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://lostfound-backend-8u9x.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
   timeout: 15000,
 })
