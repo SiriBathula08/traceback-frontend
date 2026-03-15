14478f472b6ba9077ac6a3b135508bfa03a78065
Siri Bathula <siribathula8228@gmail.com>
2026-03-15 22:53:35 +0530

    changed api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,10 +5,17 @@ const api = axios.create({
   headers: { 'Content-Type': 'application/json' },
   timeout: 15000,
 })
-
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('tb_token')
-  if (token) config.headers.Authorization = `Bearer ${token}`
+  const url = config.url || ''
+
+  const isAuthRequest =
+    url.includes('/auth/login') || url.includes('/auth/register')
+
+  if (token && !isAuthRequest) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+
   return config
 }, Promise.reject)
 
